cb685040b1738465dc17b61cf501f91356f147ea
Olivier Tardieu <tardieu@users.noreply.github.com>
2026-06-08 14:05:43 -0400

    Reuse synthetic vars across tensors for dims of size 1 (#2568)
    * Reuse synthetic vars accross tensors for dims of size 1
    
    Signed-off-by: Olivier Tardieu <tardieu@us.ibm.com>
    
    * Add test
    
    Signed-off-by: Olivier Tardieu <tardieu@us.ibm.com>
    
    * Copy new_vars to avoid skipping var indexes
    
    Signed-off-by: Olivier Tardieu <tardieu@us.ibm.com>
    
    * Implement claude suggestions
    
    Signed-off-by: Olivier Tardieu <tardieu@us.ibm.com>
    
    ---------
    
    Signed-off-by: Olivier Tardieu <tardieu@us.ibm.com>
    Signed-off-by: Meena soundararajan <meena.soundararajan@ibm.com>

--- tests/inductor/test_inductor_ops.py
+++ tests/inductor/test_inductor_ops.py
@@ -2971,6 +2971,11 @@ class TestOps(unittest.TestCase, metaclass=ParameterizedTestMeta):
                 "3d2s2": (2, 2, cached_randn((3, 3, 192), dtype=torch.float16)),
             },
         },
+        ("test_slice_synthetic_dims", "test_slice_synthetic_dims_cpu"): {
+            "param_sets": {
+                "5d": (cached_randn((2, 3, 4, 5, 192), dtype=torch.float16),),
+            },
+        },
         ("test_rope_fms", "test_rope_cpu"): {
             "param_sets": {
                 "prefill_bs1": (
@@ -5508,6 +5513,12 @@ class TestOps(unittest.TestCase, metaclass=ParameterizedTestMeta):
                 return op(dim, x[:, start:end])
             elif dim == 2:
                 return op(dim, x[:, :, start:end])
+
+        self.compare_with_cpu(fn, x, clone_inputs=True, run_eager=False)
+
+    def test_slice_synthetic_dims_cpu(self, x):
+        def fn(x):
+            return x[:, 1:2, :, :, :] + x[:, :, 2:3, :, :]
 
         self.compare_with_cpu(fn, x, clone_inputs=True, run_eager=False)
 
